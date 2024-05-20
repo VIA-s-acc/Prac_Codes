467d09d78b322852155fa070807ccc8128d7cd73
Georgii <99951336+VIA-s-acc@users.noreply.github.com>
2024-05-20 12:00:15 +0400

    fix example

--- Numintegrate/main.py
+++ Numintegrate/main.py
@@ -2,6 +2,6 @@ from NumIntegrate import *
 from math import sin
 func = lambda x: x**2
 a = Integrate(func=func, interv=(0, 2), n = 1000)
-Integral = a.squqqares('center')
+Integral = a.squares('center')
 
 print(Integral)
