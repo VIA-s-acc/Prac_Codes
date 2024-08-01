ac60ebc5d299b3c8b1648ff8fe4e23298950f886
Georgii <99951336+VIA-s-acc@users.noreply.github.com>
2024-08-01 17:22:57 +0400

    build.bat script fix

--- optimized/build.bat
+++ optimized/build.bat
@@ -1,29 +1,40 @@
 @echo off
-REM Сборка matrix_methods
+setlocal
 
-if exist lineq\matrix_methods\build rmdir /S /Q lineq\matrix_methods\build 
-if exist lineq\matrix_methods\lowlevel\matrix_methods.c del /Q lineq\matrix_methods\lowlevel\matrix_methods.c
-python lineq\matrix_methods\setup.py build_ext -b build
-move /Y build lineq\matrix_methods\build
-move /Y lineq\matrix_methods\matrix_methods.c lineq\matrix_methods\lowlevel\matrix_methods.c
+REM проверка на то, что это Windows или Linux
+if "%OS%"=="Windows_NT" (
+    set "IS_WINDOWS=true"
+) else (
+    set "IS_WINDOWS=false"
+)
 
+REM цикл по всем модулям
+for %%i in (matrix_methods generator checker) do (
+    REM удаление каталога build
+    if exist lineq\%%i\build (
+        if "%IS_WINDOWS%"=="true" (
+            rmdir /S /Q lineq\%%i\build
+        ) else (
+            rm -rf lineq/%%i/build
+        )
+    )
+    REM удаление файла lowlevel/matrix_methods.c
+    if exist lineq\%%i\lowlevel\%%i.c (
+        del /Q lineq\%%i\lowlevel\%%i.c
+    )
+    REM сборка
+    python lineq\%%i\setup.py build_ext -b build
+    REM перемещение каталога build
+    if "%IS_WINDOWS%"=="true" (
+        move /Y build lineq\%%i\build
+    ) else (
+        mv build lineq/%%i/build
+    )
+    REM перемещение файла matrix_methods.c
+    if "%IS_WINDOWS%"=="true" (
+        move /Y lineq\%%i\%%i.c lineq\%%i\lowlevel\%%i.c
+    ) else (
+        mv lineq/%%i/%%i.c lineq/%%i/lowlevel/%%i.c
+    )
+)
 
-
-REM сборка generator
-
-if exist lineq\generator\build rmdir /S /Q lineq\generator\build 
-if exist lineq\generator\lowlevel\generator.c del /Q lineq\generator\lowlevel\generator.c
-python lineq\generator\setup.py build_ext -b build
-move /Y build lineq\generator\build
-move /Y lineq\generator\generator.c lineq\generator\lowlevel\generator.c
-
-
-
-
-REM сборка checker
-
-if exist lineq\checker\build rmdir /S /Q lineq\checker\build 
-if exist lineq\checker\lowlevel\checker.c del /Q lineq\checker\lowlevel\checker.c
-python lineq\checker\setup.py build_ext -b build
-move /Y build lineq\checker\build
-move /Y lineq\checker\checker.c lineq\checker\lowlevel\checker.c
